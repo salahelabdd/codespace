4a8bc4135d82ea524ee49641c04bb23940d0434f
salahelabdd <144468027+salahelabdd@users.noreply.github.com>
2023-09-08 15:31:39 +0000

    Fri, Sep 8, 2023, 3:31 PM +00:00

--- compare.c
+++ compare.c
@@ -1,1 +1,13 @@
-incude<stdio.h>
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int x = get_int("what x ");
+    int y = get_int ("whats y ");
+
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+}

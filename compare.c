27b40ae1e6e3034297510777db3919613ce753e8
salahelabdd <144468027+salahelabdd@users.noreply.github.com>
2023-09-08 15:34:57 +0000

    Fri, Sep 8, 2023, 3:34 PM +00:00

--- compare.c
+++ compare.c
@@ -10,4 +10,12 @@ int main(void)
     {
         printf("x is less than y\n");
     }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
 }
